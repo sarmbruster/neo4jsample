42c4458edc08706e3210c4eaed5991ac495fabfd
Stefan Armbruster <stefan@armbruster-it.de>
2012-02-15 01:13:41 +0100

    adding sample for traversal

--- grails-app/controllers/sample/neo4j/SpeakerController.groovy
+++ grails-app/controllers/sample/neo4j/SpeakerController.groovy
@@ -1,7 +1,26 @@
 package sample.neo4j
 
+import org.neo4j.graphdb.TraversalPosition
+import org.neo4j.graphdb.StopEvaluator
+import org.neo4j.graphdb.ReturnableEvaluator
+
 class SpeakerController {
 
     def scaffold = true
 
+    def search() {
+        render Speaker.where { name=='Peter'}.list()
+    }
+
+    def traverse() {
+        render Speaker.traverseStatic(
+
+                { TraversalPosition tp ->
+                    //log.error "${tp.depth()}, ${tp.currentNode().id}"
+                    tp.depth() in (1..2)
+                } as StopEvaluator,
+
+                ReturnableEvaluator.ALL_BUT_START_NODE
+        )
+    }
 }
