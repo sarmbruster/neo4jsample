4df51941b88b118e8566f69c0f25406c6897ac93
Stefan Armbruster <stefan@armbruster-it.de>
2012-02-27 12:16:39 +0100

    workaround for SpringSource/grails-data-mapping#25

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,13 @@
 // Place your Spring DSL code here
 beans = {
+
+    // workaround for https://github.com/SpringSource/grails-data-mapping/issues/25
+    graphDatabaseService(
+            org.neo4j.rest.graphdb.RestGraphDatabase,
+            "http://${System.env['NEO4J_HOST']}:${System.env['NEO4J_PORT']}/db/data",
+            System.env['NEO4J_LOGIN'],
+            System.env['NEO4J_PASSWORD']
+    ) { bean ->
+        bean.destroyMethod = "shutdown"
+    }
 }
