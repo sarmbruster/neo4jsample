339a52911b0cb64cc4688e0e4a7e481e864bb7c9
Stefan Armbruster <stefan@armbruster-it.de>
2012-09-07 22:25:24 +0200

    couple of version upgrades: Grails 2.1.0, Neo4j 1.8.M07, Plugin 1.0.0.M2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,7 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         inherits true
+        mavenLocal()
         grailsPlugins()
         grailsHome()
         grailsCentral()
@@ -20,29 +21,34 @@ grails.project.dependency.resolution = {
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //flatDir name:'myRepo', dirs:'/abc/def'
-        mavenLocal()
         mavenCentral()
         mavenRepo 'http://m2.neo4j.org/releases'
 //        mavenRepo 'http://tinkerpop.com/maven2'
     }
 
+    neo4jVerison="1.8.M07"
     dependencies {
 
-        // uncomment next three lines if you're using embedded/ha *and* you want the webadmin available
-//        compile(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1")
-//        runtime(group:"org.neo4j.app", name:"neo4j-server", version:"1.6.1", branch:"static-web")
-//        runtime('com.sun.jersey:jersey-bundle:1.9')
+        compile("org.neo4j:neo4j-community:$neo4jVerison")
 
+        // uncomment next three lines if you're using embedded/ha *and* you want the webadmin available
+        /*compile(group:"org.neo4j.app", name:"neo4j-server", version:neo4jVerison)
+        runtime(group:"org.neo4j.app", name:"neo4j-server", version:neo4jVerison, branch:"static-web")
+			//runtime (group:"org.neo4j", name:"neo4j-shell", version:""1.8.M07")
+        runtime('com.sun.jersey:jersey-server:1.9')
+        runtime('com.sun.jersey:jersey-core:1.9')*/
 
         // uncomment following line if type=rest is used in DataSource.groovy
-        compile("org.neo4j:neo4j-rest-graphdb:1.6")
+        //compile "org.neo4j:neo4j-rest-graphdb:1.8.M07"
+
+        //test 'org.databene:contiperf:2.0.0'
     }
 
 	 plugins {
-         //runtime ":neo4j:1.0.0.M1"
-         //runtime ":neo4j:1.0.0.SNAPSHOT"
+         runtime ":neo4j:1.0.0.M2"
          runtime ":jquery:1.7.1"
          runtime ":resources:1.1.6"
+         runtime ":spock:0.6"
 
          // Uncomment these (or add new ones) to enable additional resources capabilities
          //runtime ":zipped-resources:1.0"
