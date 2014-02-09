2d113a1e4f809ee715b2b35ee4c62c3821954877
Stefan Armbruster <stefan@armbruster-it.de>
2014-02-09 12:08:26 +0100

    seperate action for cypher

--- grails-app/controllers/sample/neo4j/CypherController.groovy
+++ grails-app/controllers/sample/neo4j/CypherController.groovy
@@ -3,7 +3,14 @@ package sample.neo4j
 class CypherController {
 
     def index() {
+        forward action:"cypher"
+
+    }
+
+    def cypher() {
         response.contentType = "text/plain"
         render Speaker.cypherStatic("start n=node({this}) match n-[:INSTANCE]->m return m")
     }
+
+
 }
